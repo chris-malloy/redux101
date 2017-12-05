ed59513709757a478ceebb35e697d022bda2d4f4
chris-malloy <chrismalloymusic@gmail.com>
2017-12-05 10:29:21 -0500

    created reducers

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+// store comes from redux
+import { createStore } from 'redux';
+// provider component connects react to redux
+import { Provider } from 'react-redux';
+// main reducer (rootReducer) will be handed to store
+import reducers from './reducers/index';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+// master reducer,  does not manage state, container all pieces of state from child reducers
+import { combineReducers } from 'redux';
+// import reducers --> must provide at least one
+import StudentReducer from './StudentReducer';
+// build root reducer
+// combineReducers takes 1 arg, an object
+const rootReducer = combineReducers({
+    // name of state is property (used by react)
+    students: StudentReducer,
+})
+export default rootReducer;
+
+
